f17b3fb23488e8fc4219bc1d7a81c96501c743fc
양승권 <ysk102706@gmail.com>
2025-04-23 10:45:32 +0900

    Init

---
+++ Source/PotatoRider_Drift/PotatoRider_Drift.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PotatoRider_Drift.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PotatoRider_Drift, "PotatoRider_Drift" );
